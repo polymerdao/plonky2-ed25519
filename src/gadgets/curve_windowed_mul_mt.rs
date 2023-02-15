989ab18a6cc3465b2e12ceab3ccac06d3af1018e
Sai Deng <dengsai@hotmail.com>
2023-02-14 20:40:18 -0800

    working on recursion circuits

--- src/gadgets/curve_windowed_mul_mt.rs
+++ src/gadgets/curve_windowed_mul_mt.rs
@@ -10,12 +10,12 @@ use plonky2::iop::target::Target;
 use plonky2::iop::witness::{PartialWitness, WitnessWrite};
 use plonky2::plonk::circuit_builder::CircuitBuilder;
 use plonky2::plonk::circuit_data::{
-    CircuitConfig, CommonCircuitData, VerifierCircuitTarget, VerifierOnlyCircuitData,
+    CircuitConfig, CircuitData, CommonCircuitData, VerifierCircuitTarget, VerifierOnlyCircuitData,
 };
 use plonky2::plonk::config::{AlgebraicHasher, GenericConfig, GenericHashOut, Hasher};
-use plonky2::plonk::proof::ProofWithPublicInputs;
+use plonky2::plonk::proof::{ProofWithPublicInputs, ProofWithPublicInputsTarget};
 use plonky2::plonk::prover::prove;
-use plonky2_ecdsa::gadgets::biguint::{BigUintTarget, WitnessBigUint};
+use plonky2_ecdsa::gadgets::biguint::WitnessBigUint;
 use plonky2_field::extension::Extendable;
 use plonky2_field::types::{Field, PrimeField, Sample};
 use plonky2_u32::gadgets::arithmetic_u32::{CircuitBuilderU32, U32Target};
@@ -44,6 +44,92 @@ pub struct CurveScalarMulWindowedPartTarget<CV: Curve> {
     pub p_target: AffinePointTarget<CV>,
     pub p_init_target: AffinePointTarget<CV>,
     pub n_target: NonNativeTarget<CV::ScalarField>,
+    pub q_target: AffinePointTarget<CV>,
+}
+
+pub fn register_public_affine_point_target<
+    F: RichField + Extendable<D>,
+    CV: Curve,
+    C: GenericConfig<D, F = F>,
+    const D: usize,
+>(
+    builder: &mut CircuitBuilder<F, D>,
+    target: &AffinePointTarget<CV>,
+) {
+    for x in target.x.value.limbs {
+        builder.register_public_input(x.0);
+    }
+    for y in target.y.value.limbs {
+        builder.register_public_input(y.0);
+    }
+}
+
+pub fn register_public_nonnative_target<
+    F: RichField + Extendable<D>,
+    CV: Curve,
+    C: GenericConfig<D, F = F>,
+    const D: usize,
+>(
+    builder: &mut CircuitBuilder<F, D>,
+    target: &NonNativeTarget<CV::ScalarField>,
+) {
+    for x in target.value.limbs {
+        builder.register_public_input(x.0);
+    }
+}
+
+pub fn load_curve_scalar_mul_windowed_part_circuit_public_inputs_target<
+    F: RichField + Extendable<D>,
+    CV: Curve,
+    C: GenericConfig<D, F = F>,
+    const D: usize,
+>(
+    builder: &mut CircuitBuilder<F, D>,
+    public_input_targets: Vec<Target>,
+) -> CurveScalarMulWindowedPartTarget<CV> {
+    let p_target = builder.add_virtual_affine_point_target::<CV>();
+    let p_init_target = builder.add_virtual_affine_point_target::<CV>();
+    let n_target = builder.add_virtual_nonnative_target();
+    let q_target =
+        builder.curve_scalar_mul_windowed_part(NUM_LIMBS, &p_init_target, &p_target, &n_target);
+
+    let mut index = 0;
+    for x in p_target.x.value.limbs {
+        builder.connect(public_input_targets[index], x.0);
+        index = index + 1;
+    }
+    for y in p_target.y.value.limbs {
+        builder.connect(public_input_targets[index], y.0);
+        index = index + 1;
+    }
+    for x in p_init_target.x.value.limbs {
+        builder.connect(public_input_targets[index], x.0);
+        index = index + 1;
+    }
+    for y in p_init_target.y.value.limbs {
+        builder.connect(public_input_targets[index], y.0);
+        index = index + 1;
+    }
+    for x in n_target.value.limbs {
+        builder.connect(public_input_targets[index], x.0);
+        index = index + 1;
+    }
+    for x in q_target.x.value.limbs {
+        builder.connect(public_input_targets[index], x.0);
+        index = index + 1;
+    }
+    for y in q_target.y.value.limbs {
+        builder.connect(public_input_targets[index], y.0);
+        index = index + 1;
+    }
+    assert_eq!(index - 1, public_input_targets.len());
+
+    CurveScalarMulWindowedPartTarget {
+        p_target,
+        p_init_target,
+        n_target,
+        q_target,
+    }
 }
 
 pub fn build_curve_scalar_mul_windowed_part_circuit<
@@ -57,23 +143,41 @@ pub fn build_curve_scalar_mul_windowed_part_circuit<
     let p_target = builder.add_virtual_affine_point_target::<CV>();
     let p_init_target = builder.add_virtual_affine_point_target::<CV>();
     let n_target = builder.add_virtual_nonnative_target();
-    let res_target =
+    let q_target =
         builder.curve_scalar_mul_windowed_part(NUM_LIMBS, &p_init_target, &p_target, &n_target);
-    for x in res_target.x.value.limbs {
-        builder.register_public_input(x.0);
-    }
-    for y in res_target.y.value.limbs {
-        builder.register_public_input(y.0);
-    }
+
+    register_public_affine_point_target(builder, &p_target);
+    register_public_affine_point_target(builder, &p_init_target);
+    register_public_nonnative_target(builder, &n_target);
+    register_public_affine_point_target(builder, &q_target);
+
     CurveScalarMulWindowedPartTarget {
         p_target,
         p_init_target,
         n_target,
+        q_target,
     }
 }
 
-// init_p should match other sub circuit
-pub fn prove_windowed_mul_sub<
+pub fn get_curve_scalar_mul_windowed_part_circuit_data<
+    F: RichField + Extendable<D>,
+    CV: Curve,
+    C: GenericConfig<D, F = F>,
+    const D: usize,
+>(
+    config: CircuitConfig,
+) -> Result<CircuitData<F, C, D>>
+where
+    [(); C::Hasher::HASH_SIZE]:,
+    <C as GenericConfig<D>>::Hasher: AlgebraicHasher<F>,
+{
+    let mut builder = CircuitBuilder::<F, D>::new(config);
+    let _ = build_curve_scalar_mul_windowed_part_circuit::<F, CV, C, D>(&mut builder);
+    let data = builder.build::<C>();
+    Ok(data)
+}
+
+pub fn prove_curve_scalar_mul_windowed_part<
     F: RichField + Extendable<D>,
     CV: Curve,
     C: GenericConfig<D, F = F>,
@@ -108,6 +212,86 @@ where
     let proof = data.prove(pw).unwrap();
 
     Ok((proof, data.verifier_only, data.common))
+}
+
+pub struct CurveScalarMulMtData<
+    'a,
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+    const D: usize,
+> {
+    pub proof0: &'a ProofWithPublicInputsTarget<D>,
+    pub proof1: &'a ProofWithPublicInputsTarget<D>,
+    pub p_target: &'a AffinePointTarget<CV>,
+    pub n_target: &'a NonNativeTarget<CV::ScalarField>,
+    pub q_target: &'a AffinePointTarget<CV>,
+}
+
+pub fn build_curve_scalar_mul_windowed_mt_circuit<
+    F: RichField + Extendable<D>,
+    CV: Curve,
+    C: GenericConfig<D, F = F>,
+    const D: usize,
+>(
+    config: CircuitConfig,
+    builder: &mut CircuitBuilder<F, D>,
+) -> Result<CurveScalarMulMtData<F, C, D>> {
+    let circuit_data = get_curve_scalar_mul_windowed_part_circuit_data(config.clone())?;
+
+    let proof0 = builder.add_virtual_proof_with_pis::<C>(&circuit_data.common);
+    let proof1 = builder.add_virtual_proof_with_pis::<C>(&circuit_data.common);
+
+    builder.verify_proof::<C>(&proof0, &&circuit_data.verifier_only, &circuit_data.common);
+    builder.verify_proof::<C>(&proof1, &&circuit_data.verifier_only, &circuit_data.common);
+
+    let proof0_targets = load_curve_scalar_mul_windowed_part_circuit_public_inputs_target(
+        builder,
+        proof0.public_inputs,
+    );
+    let proof1_targets = load_curve_scalar_mul_windowed_part_circuit_public_inputs_target(
+        builder,
+        proof1.public_inputs,
+    );
+
+    let p_target = builder.add_virtual_affine_point_target();
+    let n_target = builder.add_virtual_nonnative_target();
+    let q_target = builder.add_virtual_affine_point_target();
+
+    builder.connect_affine_point(&p_target, &proof0_targets.p_target);
+    let limb_count = n_target.value.limbs.len();
+    for i in 0..limb_count / 2 {
+        builder.connect_u32(
+            n_target.value.limbs[i],
+            proof0_targets.n_target.value.limbs[i],
+        );
+    }
+    for i in limb_count / 2..limb_count {
+        builder.connect(
+            proof0_targets.n_target.value.limbs[i].0,
+            builder.constant(F::from_canonical_usize(0)),
+        );
+    }
+    for i in 0..limb_count / 2 {
+        builder.connect_u32(
+            n_target.value.limbs[i + limb_count / 2],
+            proof1_targets.n_target.value.limbs[i],
+        );
+    }
+    for i in limb_count / 2..limb_count {
+        builder.connect(
+            proof1_targets.n_target.value.limbs[i].0,
+            builder.constant(F::from_canonical_usize(0)),
+        );
+    }
+    builder.connect_affine_point(&q_target, &proof1_targets.q_target);
+
+    Ok(CurveScalarMulMtData {
+        proof0: &proof0,
+        proof1: &proof1,
+        p_target: &p_target,
+        n_target: &n_target,
+        q_target: &q_target,
+    })
 }
 
 pub fn prove_curve25519_mul_mt<
@@ -152,8 +336,12 @@ where
     let mut builder = CircuitBuilder::<F, D>::new(config.clone());
     let mut pw = PartialWitness::new();
     {
-        let (inner_proof, inner_vd, inner_cd) =
-            prove_windowed_mul_sub::<F, Ed25519, C, D>(config.clone(), &p, &n0, &p0_init)?;
+        let (inner_proof, inner_vd, inner_cd) = prove_curve_scalar_mul_windowed_part::<
+            F,
+            Ed25519,
+            C,
+            D,
+        >(config.clone(), &p, &n0, &p0_init)?;
         let pt = builder.add_virtual_proof_with_pis::<InnerC>(&inner_cd);
         pw.set_proof_with_pis_target(&pt, &inner_proof);
 
@@ -171,8 +359,12 @@ where
     }
 
     {
-        let (inner_proof, inner_vd, inner_cd) =
-            prove_windowed_mul_sub::<F, Ed25519, C, D>(config.clone(), &p1, &n1, &p1_init)?;
+        let (inner_proof, inner_vd, inner_cd) = prove_curve_scalar_mul_windowed_part::<
+            F,
+            Ed25519,
+            C,
+            D,
+        >(config.clone(), &p1, &n1, &p1_init)?;
         let pt = builder.add_virtual_proof_with_pis::<InnerC>(&inner_cd);
         pw.set_proof_with_pis_target(&pt, &inner_proof);
 
